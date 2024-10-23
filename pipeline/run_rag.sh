3b818adce0dab415f59b13caff06729525bbb3a8
Qingyang Liu <liuqingy@umich.edu>
2024-10-23 15:37:33 -0400

    add commands in bash files

--- pipeline/run_rag.sh
+++ pipeline/run_rag.sh
@@ -2,60 +2,60 @@
 # example usage: bash pipeline/run_rag.sh
 
 # strategy 0: default setting
-# python pipeline/rag_pipeline_new.py \
-#     --model_name meta-llama/Llama-3.1-8B-Instruct \
-#     --dtype float16 \
-#     --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
-#     --embedding_dim 384 \
-#     --splitter_type recursive \
-#     --chunk_size 1000 \
-#     --chunk_overlap 200 \
-#     --text_files_path data/crawled/crawled_text_data \
-#     --top_k_search 3 \
-#     --retriever_type CHROMA\
-#     --output_file output/llama3_recursive_chroma_top3.csv \
+python pipeline/rag_pipeline_new.py \
+    --model_name meta-llama/Llama-3.1-8B-Instruct \
+    --dtype float16 \
+    --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
+    --embedding_dim 384 \
+    --splitter_type recursive \
+    --chunk_size 1000 \
+    --chunk_overlap 200 \
+    --text_files_path data/crawled/crawled_text_data \
+    --top_k_search 3 \
+    --retriever_type CHROMA\
+    --output_file output/llama3_recursive_chroma_top3.csv \
 
-# # strategy 1: recursive splitter, CHROMA retriever, tune chunk size
-# python pipeline/rag_pipeline_new.py \
-#     --model_name meta-llama/Llama-3.1-8B-Instruct \
-#     --dtype float16 \
-#     --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
-#     --embedding_dim 384 \
-#     --splitter_type recursive \
-#     --chunk_size 1500 \
-#     --chunk_overlap 200 \
-#     --text_files_path data/crawled/crawled_text_data \
-#     --top_k_search 3 \
-#     --retriever_type CHROMA\
-#     --output_file output/llama3_recursive_chunk1500_chroma_top3_sample100.csv \
+# strategy 1: recursive splitter, CHROMA retriever, tune chunk size
+python pipeline/rag_pipeline_new.py \
+    --model_name meta-llama/Llama-3.1-8B-Instruct \
+    --dtype float16 \
+    --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
+    --embedding_dim 384 \
+    --splitter_type recursive \
+    --chunk_size 1500 \
+    --chunk_overlap 200 \
+    --text_files_path data/crawled/crawled_text_data \
+    --top_k_search 3 \
+    --retriever_type CHROMA\
+    --output_file output/llama3_recursive_chunk1500_chroma_top3_sample100.csv \
 
 # strategy 2: CHROMA retriever, tune splitter
-# python pipeline/rag_pipeline_new.py \
-#     --model_name meta-llama/Llama-3.1-8B-Instruct \
-#     --dtype float16 \
-#     --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
-#     --embedding_dim 384 \
-#     --splitter_type semantic \
-#     --chunk_size 1000 \
-#     --chunk_overlap 200 \
-#     --text_files_path data/crawled/crawled_text_data \
-#     --top_k_search 3 \
-#     --retriever_type CHROMA\
-#     --output_file output/llama3_semantic_chroma_top3_sample100.csv \
+python pipeline/rag_pipeline_new.py \
+    --model_name meta-llama/Llama-3.1-8B-Instruct \
+    --dtype float16 \
+    --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
+    --embedding_dim 384 \
+    --splitter_type semantic \
+    --chunk_size 1000 \
+    --chunk_overlap 200 \
+    --text_files_path data/crawled/crawled_text_data \
+    --top_k_search 3 \
+    --retriever_type CHROMA\
+    --output_file output/llama3_semantic_chroma_top3_sample100.csv \
 
 # strategy 3: change retriever: FAISS
-# python pipeline/rag_pipeline_new.py \
-#     --model_name meta-llama/Llama-3.1-8B-Instruct \
-#     --dtype float16 \
-#     --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
-#     --embedding_dim 384 \
-#     --splitter_type recursive \
-#     --chunk_size 1000 \
-#     --chunk_overlap 200 \
-#     --text_files_path data/crawled/crawled_text_data \
-#     --top_k_search 3 \
-#     --retriever_type FAISS \
-#     --output_file output/tests/llama3_faiss_test_reverseprompt.csv \
+python pipeline/rag_pipeline_new.py \
+    --model_name meta-llama/Llama-3.1-8B-Instruct \
+    --dtype float16 \
+    --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
+    --embedding_dim 384 \
+    --splitter_type recursive \
+    --chunk_size 1000 \
+    --chunk_overlap 200 \
+    --text_files_path data/crawled/crawled_text_data \
+    --top_k_search 3 \
+    --retriever_type FAISS \
+    --output_file output/tests/llama3_faiss_test_reverseprompt.csv \
 
 
 # strategy 4: tune reranking using faiss
@@ -78,17 +78,17 @@ python pipeline/rag_pipeline_new.py \
 
 
 # strategy 5: add hqde for better retrieval
-# python pipeline/rag_pipeline_new.py \
-#     --model_name meta-llama/Llama-3.1-8B-Instruct \
-#     --dtype float16 \
-#     --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
-#     --embedding_dim 384 \
-#     --splitter_type recursive \
-#     --chunk_size 1000 \
-#     --chunk_overlap 200 \
-#     --text_files_path data/crawled/crawled_text_data \
-#     --top_k_search 3 \
-#     --retriever_type FAISS \
-#     --rerank_model_name ms-marco-MiniLM-L-12-v2 \
-#     --hypo True \
-#     --output_file output/llama3_faiss_test_hypo_promptENG3.csv \
+python pipeline/rag_pipeline_new.py \
+    --model_name meta-llama/Llama-3.1-8B-Instruct \
+    --dtype float16 \
+    --embedding_model_name sentence-transformers/all-MiniLM-L6-v2 \
+    --embedding_dim 384 \
+    --splitter_type recursive \
+    --chunk_size 1000 \
+    --chunk_overlap 200 \
+    --text_files_path data/crawled/crawled_text_data \
+    --top_k_search 3 \
+    --retriever_type FAISS \
+    --rerank_model_name ms-marco-MiniLM-L-12-v2 \
+    --hypo True \
+    --output_file output/llama3_faiss_test_hypo_promptENG3.csv \
